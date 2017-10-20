c6b02110c37cde94f2944458ad629c6fc0cd74f1
blevesque <basteos76@gmail.com>
2017-10-20 19:08:13 +0200

    fix(backend): minor fixes

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
+// npm modules
 var app = require('express')();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var ent = require('ent');
 var fs = require('fs');
 
+// Dynamic routes
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendfile(__dirname + '/chat.html');
 });
 
 io.sockets.on('connection', function (socket, pseudo) {
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    // As soon as we receive a pseudo, stock it in a var and send it to other users (new user joined channel)
     socket.on('new_client', function(pseudo) {
         pseudo = ent.encode(pseudo);
         socket.pseudo = pseudo;
         socket.broadcast.emit('new_client', pseudo);
     });
 
-    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    // As soon as we receive a message, we broadcast it to every users
     socket.on('message', function (message) {
         message = ent.encode(message);
         socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
     }); 
 });
 
-server.listen(8080);
+// Server startup 
+var port = 8080;
+server.listen(port); // Localhost
+console.log('Server started on port ' + port + '.');
